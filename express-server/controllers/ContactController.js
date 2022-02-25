67409a855c28e1d9ba60437906b80859ab437780
KevinDeVadder <devadderkevin@gmail.com>
2022-02-25 12:10:18 +0200

    feat(express api): :sparkles: Added Adding Contacts Controller

--- express-server/controllers/ContactController.js
+++ express-server/controllers/ContactController.js
@@ -33,7 +33,29 @@ module.exports = {
         }
     },
     async addOne(req, res, next){
-
+        try{
+            //Check if contact already exists
+            const contact = await ContactModel.findOne({owner: req.body.userId, $or: {email: req.body.email, phoneNumber: req.body.phoneNumber}})
+            if(!contact){
+                const newContact = await ContactModel.create({
+                    name: req.body.name,
+                    email: req.body.email,
+                    phoneNumber: req.body.phoneNumber,
+                    profilePicture: req.files[0].fileName
+                })
+                //Send Contact JSON
+                res.send(newContact)
+            }
+            else{
+                res.status(400).send({error: "Contact already exists"})
+            }
+        }
+        catch(err){
+            //Log Error and send it to middleware
+            //TODO: Add more expressive errors
+            console.log(err)
+            next(err)
+        }
     },
     async editOne(req, res, next){
 
