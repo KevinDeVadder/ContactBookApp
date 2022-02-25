6bea3796edda783b77d6c180cfa3e412d310ad0b
KevinDeVadder <devadderkevin@gmail.com>
2022-02-25 04:10:08 +0200

    test(express api): :white_check_mark: Added Contact Test descriptions

--- express-server/index.test.js
+++ express-server/index.test.js
@@ -35,7 +35,7 @@ describe('Users API', () => {
                     email: 'test@test.com'
                 })
             )
-            //Test if response contains JTW cookie
+            //Test if response contains JWT cookie
             expect( response.get('Set-Cookie') ).toBeDefined()
         })
     })
@@ -53,4 +53,39 @@ describe('Users API', () => {
             password: '12345678'
         }).expect(403)
     })
+})
+
+describe('Contacts API', () => {
+    it('List all Contacts of user with no login --> 403', () => {})
+    it('Login User', () => {
+        //Try to login
+        return request(app).post('/api/v1/authenticate').send({
+            email: 'test@test.com',
+            password: 'as12345678'
+        }).expect(200).then((response) => {
+            //Test if response contains name and email
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    name: 'John Doe',
+                    email: 'test@test.com'
+                })
+            )
+            //Test if response contains JWT cookie
+            expect( response.get('Set-Cookie') ).toBeDefined()
+        })
+    })
+    it('Add one contact of user', () => {})
+    it('List all Contacts of user', () => {})
+    it('See one contact of user', () => {})
+    it('See one contact of user that does not exist --> 404', () => {})
+    it('Add one contact of user that already exists', () => {})
+    it('Add one contact of user but with missing data', () => {})
+    it('Add one contact of user but with wrong data', () => {})
+    it('Edit contact', () => {})
+    it('Edit contact with bad name', () => {})
+    it('Edit contact with bad email', () => {})
+    it('Edit contact with bad phoneNumber', () => {})
+    it('Edit contact with bad profilePicture', () => {})
+    it('Delete contact', () => {})
+    it('Delete contact that does not exist --> 404', () => {})
 })
