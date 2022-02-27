60d8c01ba002915691f35313d7cba4c9c625b1e9
KevinDeVadder <devadderkevin@gmail.com>
2022-02-28 01:48:05 +0200

    feat(vue-client): :sparkles: Added Header Component (& Installed Vuex)

---
+++ contactbook-client/src/components/general/header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <!-- System Bar -->
+        <v-system-bar color="purple darken-1"></v-system-bar>
+
+        <!-- Actual header -->
+        <v-app-bar
+        absolute
+        color="purple darken-2"
+        dark
+        dense
+        >
+            <!-- Header -->
+            <v-toolbar-title>ContactMe</v-toolbar-title>
+            
+            <!-- Spacer -->
+            <v-spacer/>
+
+            <!-- Log Out Button -->
+            <v-btn color="purple lighten-2 dark" v-if="isUserLoggedIn">Log out</v-btn>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            isUserLoggedIn: false
+        }
+    }
+}
+</script>
