9e7ded4928de8c093c5b03d7659688713475453e
KevinDeVadder <devadderkevin@gmail.com>
2022-02-28 02:31:33 +0200

    feat(vue-client): :sparkles: Added Login Form

--- contactbook-client/src/components/forms/FormLayout.vue
+++ contactbook-client/src/components/forms/FormLayout.vue
@@ -1,10 +1,14 @@
 <template>
     <!-- Layout for the Login and Register Forms -->
-    <v-layout justify-center>
+    <v-container fill-height>
         <!-- Grid for the Login and Register Forms  -->
-        <v-flex xs12 sm10 md8 lg6>
+        <v-row align="center" justify="center">
             <!-- Form -->
-            <v-card ref="form">
+            <v-card :width="width" :height="height" ref="form" class="pa-5">
+                <!-- Card title -->
+                <v-card-title class="purple--text">
+                    <slot name="title"></slot>
+                </v-card-title>
                 <!-- Form fields -->
                 <v-card-text>
                     <slot name="fields"></slot>
@@ -14,6 +18,34 @@
                     <slot name="actions"></slot>
                 </v-card-actions>
             </v-card>
-        </v-flex>
-  </v-layout>
+        </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+    computed: {
+        //Code source: https://vuetifyjs.com/en/features/breakpoints/#breakpoint-service
+        height () {
+            switch (this.$vuetify.breakpoint.name) {
+                case 'xs': return 500
+                case 'sm': return 500
+                case 'md': return 500
+                case 'lg': return '40%'
+                case 'xl': return '40%'
+            }
+            return 0
+        },
+        width () {
+            switch (this.$vuetify.breakpoint.name) {
+                case 'xs': return '80%'
+                case 'sm': return '80%'
+                case 'md': return '80%'
+                case 'lg': return '50%'
+                case 'xl': return '50%'
+            }
+            return 0
+        }
+    },
+}
+</script>
--- contactbook-client/src/components/forms/LoginForm.vue
+++ contactbook-client/src/components/forms/LoginForm.vue
@@ -1,6 +1,41 @@
 <template>
     <form-layout>
+        <!-- Form Title -->
+        <template v-slot:title>
+            <!-- Title -->
+            Log in into ContactMe
+        </template>
+
+        <!-- Form Fields -->
+        <template v-slot:fields>
+            <!-- Email Field -->
+            <v-text-field
+                ref="email"
+                v-model="email"
+                :rules="[() => !!email || 'This field is required']"
+                label="Email"
+                placeholder="basicemail@gmail.com"
+                required
+            ></v-text-field>
+            <!-- Password Field -->
+            <v-text-field
+                ref="password"
+                v-model="password"
+                :type="'password'"
+                :rules="[
+                () => !!password || 'This field is required'
+                ]"
+                label="Password"
+                placeholder="Password"
+                required
+            ></v-text-field>
+        </template>
         
+        <!-- Form Buttons -->
+        <template v-slot:actions>
+            <!-- Submit Button -->
+            <v-btn dark raised color="purple darken-2" @click="login">Login</v-btn>
+        </template>
     </form-layout>
 </template>
 
@@ -9,6 +44,17 @@ import formLayout from '@/components/forms/FormLayout'
 export default {
     components: {
         formLayout
+    },
+    data(){
+        return{
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async login(){
+
+        }
     }
 }
 </script>
